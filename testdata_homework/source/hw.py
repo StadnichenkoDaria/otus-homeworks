fe55896aeb0987e6ec0e15b50d799ed87acb791c
StadnichenkoDaria <brodiagad@gmail.com>
2021-12-02 19:16:13 +0300

    fixed

--- testdata_homework/source/hw.py
+++ testdata_homework/source/hw.py
@@ -1,19 +1,17 @@
 from csv import DictReader
 from json import loads, dumps
-
-result = []
+from itertools import cycle
 
 with open("../data/users.json", "r") as json_file:
     with open("../data/books.csv", "r") as csv_file:
         with open("../data/result.json", "w") as write_file:
-            people = loads(json_file.read())
-            library = DictReader(csv_file)
+            users = loads(json_file.read())
+            books = DictReader(csv_file)
 
-            for person in people:
-                result.append({'name': person.get('name'), 'gender': person.get('gender'),
-                               'address': person.get('address')})
+            for book, user in zip(books, cycle(users)):
+                book = [{'title': book.get("Title"), 'author': book.get("Author"),
+                         'pages': int(book.get("Pages")), 'genre': book.get("Genre")}]
+                user = [{'name': user.get('name'), 'gender': user.get('gender'),
+                        'address': user.get('address'), 'age': user.get('age'), 'book': book}]
 
-            for i, book in enumerate(library):
-                if i < len(result):
-                    result[i].update({"books": book})
-            write_file.write(dumps(result, indent=4))
+                write_file.write(dumps(user, indent=4))
