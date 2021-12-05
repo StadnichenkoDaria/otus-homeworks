a9b60e1bd9720085d23925fce8c5b052f6b5d64f
StadnichenkoDaria <brodiagad@gmail.com>
2021-12-05 13:24:05 +0300

    fix

--- testdata_homework/source/hw.py
+++ testdata_homework/source/hw.py
@@ -2,16 +2,22 @@ from csv import DictReader
 from json import loads, dumps
 from itertools import cycle
 
+result = []
+
 with open("../data/users.json", "r") as json_file:
     with open("../data/books.csv", "r") as csv_file:
         with open("../data/result.json", "w") as write_file:
             users = loads(json_file.read())
             books = DictReader(csv_file)
 
+            for user in users:
+                user = [{'name': user.get('name'), 'gender': user.get('gender'),
+                         'address': user.get('address'), 'age': user.get('age'), 'books': []}]
+                result.append(user)
+
             for book, user in zip(books, cycle(users)):
                 book = [{'title': book.get("Title"), 'author': book.get("Author"),
                          'pages': int(book.get("Pages")), 'genre': book.get("Genre")}]
-                user = [{'name': user.get('name'), 'gender': user.get('gender'),
-                        'address': user.get('address'), 'age': user.get('age'), 'book': book}]
+                user.books.append(book)
 
-                write_file.write(dumps(user, indent=4))
+            write_file.write(dumps(result, indent=4))
