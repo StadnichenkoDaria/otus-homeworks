834b135136568a49a968ccb91f97e7148f9742b2
StadnichenkoDaria <brodiagad@gmail.com>
2021-12-05 22:46:05 +0300

    fix

--- testdata_homework/source/hw.py
+++ testdata_homework/source/hw.py
@@ -3,7 +3,6 @@ from json import loads, dumps
 from itertools import cycle
 
 result = []
-
 with open("../data/users.json", "r") as json_file:
     with open("../data/books.csv", "r") as csv_file:
         with open("../data/result.json", "w") as write_file:
@@ -11,13 +10,13 @@ with open("../data/users.json", "r") as json_file:
             books = DictReader(csv_file)
 
             for user in users:
-                user = [{'name': user.get('name'), 'gender': user.get('gender'),
-                         'address': user.get('address'), 'age': user.get('age'), 'books': []}]
+                user = {'name': user.get('name'), 'gender': user.get('gender'), 'address': user.get('address'),
+                        'age': user.get('age'), 'books': []}
                 result.append(user)
 
-            for book, user in zip(books, cycle(users)):
-                book = [{'title': book.get("Title"), 'author': book.get("Author"),
-                         'pages': int(book.get("Pages")), 'genre': book.get("Genre")}]
-                user.books.append(book)
+            for book, user in zip(books, cycle(result)):
+                book = {'title': book.get("Title"), 'author': book.get("Author"), 'pages': int(book.get("Pages")),
+                        'genre': book.get("Genre")}
+                user['books'].append(book)
 
             write_file.write(dumps(result, indent=4))
